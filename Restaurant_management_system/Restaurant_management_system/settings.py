bd5002cf88ae972f26ffae1530545139b4655369
Darshan Parmar <67963288+darshankparmar@users.noreply.github.com>
2021-03-28 16:28:41 +0530

    SP+SEPP

--- Restaurant_management_system/Restaurant_management_system/settings.py
+++ Restaurant_management_system/Restaurant_management_system/settings.py
@@ -125,3 +125,13 @@ STATIC_URL = '/static/'
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'darshanvasaniya4841@gmail.com'
+EMAIL_HOST_PASSWORD = 'darshan123'
