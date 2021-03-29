0624a6a4b45c3cf998302974e106070898473bf3
Darshan Parmar <67963288+darshankparmar@users.noreply.github.com>
2021-03-29 10:57:13 +0530

    SP+SEPP

--- Restaurant_management_system/Restaurant_management_system/settings.py
+++ Restaurant_management_system/Restaurant_management_system/settings.py
@@ -80,6 +80,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'foodMania',  # name of the database
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
--- Restaurant_management_system/shop/admin.py
+++ Restaurant_management_system/shop/admin.py
@@ -50,5 +50,6 @@ admin.site.register(Contact, ContactAdmin)
 admin.site.register(Orders, OrdersAdmin)
 admin.site.register(OrderUpdate, OrderUpdateAdmin)
 
-admin.site.site_header = "Online Food Order"
-
+admin.site.site_header = "The Food Mania"
+admin.site.index_title = "The Food Mania Administration"
+admin.site.site_title = "The Food Mania Admin"
